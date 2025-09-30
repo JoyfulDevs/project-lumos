5891f8df466db74cf4f0b698120b53905dcd7ffe
kimyerin3456 <kimyerin0181@gmail.com>
2025-09-30 18:29:39 +0900

    slack feedback 수정본

--- cmd/test-bot/main.go
+++ cmd/test-bot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
@@ -18,6 +19,7 @@ import (
 type Handler struct {
 	appToken string
 	botToken string
+	client   *slack.Client
 }
 
 func (h *Handler) HandleEventsAPI(ctx context.Context, payload *event.EventsAPIPayload) {
@@ -28,32 +30,100 @@ func (h *Handler) HandleEventsAPI(ctx context.Context, payload *event.EventsAPIP
 
 	switch ec.Event.Type {
 	case event.EventTypeMessage:
-		if ec.Event.OfMessage.Text == "" {
+		msg := ec.Event.OfMessage
+
+		// 디버그: BotID와 User 출력
+		slog.Info("MESSAGE EVENT",
+			slog.String("user", msg.User),
+			slog.String("bot_id", msg.BotID))
+
+		// 봇 메시지 필터링
+		if msg.BotID != "" {
 			return
 		}
-		if ec.Event.OfMessage.User == ec.Event.OfMessage.ParentUserID {
+
+		if msg.Text == "" {
 			return
 		}
-		slog.Info("received message event", slog.String("text", ec.Event.OfMessage.Text))
-		c := slack.NewClient(http.DefaultClient, h.appToken, h.botToken)
-		_, err := c.PostMessage(ctx, &slack.PostMessageRequest{
-			Channel:         ec.Event.OfMessage.Channel,
-			Text:            "You said: " + ec.Event.OfMessage.Text,
-			ThreadTimestamp: ec.Event.OfMessage.ThreadTimestamp,
+		if msg.User == msg.ParentUserID {
+			return
+		}
+
+		slog.Info("USER MESSAGE - RESPONDING", slog.String("text", msg.Text))
+
+		// 스레드 타임스탬프 결정 (응답 전에)
+		threadTS := msg.ThreadTimestamp
+
+		// Block Kit을 사용한 응답 메시지 with 피드백 버튼
+		resp, err := h.client.PostMessage(ctx, &slack.PostMessageRequest{
+			Channel:         msg.Channel,
+			Text:            "You said: " + msg.Text, // 폴백 텍스트
+			ThreadTimestamp: threadTS,
+			Blocks: []slack.SlackBlock{
+				{
+					Type: "section",
+					Text: &slack.SlackText{
+						Type: "mrkdwn",
+						Text: fmt.Sprintf("*You said:* %s", msg.Text),
+					},
+				},
+				{
+					Type: "divider",
+				},
+				{
+					Type: "section",
+					Text: &slack.SlackText{
+						Type: "mrkdwn",
+						Text: "plz give us feedback",
+					},
+				},
+				{
+					Type:    "actions",
+					BlockID: "actionblock789",
+					Elements: []interface{}{
+						slack.SlackBlockElement{
+							Type: "button",
+							Text: &slack.SlackText{
+								Type:  "plain_text",
+								Text:  "good",
+								Emoji: slack.BoolPtr(true),
+							},
+							Style:    "primary",
+							Value:    "feedback_good",
+							ActionID: "feedback_good",
+						},
+						slack.SlackBlockElement{
+							Type: "button",
+							Text: &slack.SlackText{
+								Type:  "plain_text",
+								Text:  "bad",
+								Emoji: slack.BoolPtr(true),
+							},
+							Style:    "danger",
+							Value:    "feedback_bad",
+							ActionID: "feedback_bad",
+						},
+					},
+				},
+			},
 		})
 		if err != nil {
-			slog.Warn("failed to post message", slog.String("channel", ec.Event.OfMessage.Channel), slog.Any("error", err))
+			slog.Warn("failed to post message", slog.String("channel", msg.Channel), slog.Any("error", err))
+			return
 		}
+
+		slog.Info("sent message with feedback buttons", slog.String("ts", string(resp.Timestamp)))
 
 	case event.EventTypeAssistantThreadStarted:
 		slog.Info("received assistant thread started event")
 		channelID := ec.Event.OfAssistantThreadStarted.AssistantThread.ChannelID
-		c := slack.NewClient(http.DefaultClient, h.appToken, h.botToken)
+		threadTS := ec.Event.OfAssistantThreadStarted.AssistantThread.ThreadTimestamp
+
 		err := retry.Do(ctx, func(ctx context.Context) error {
-			_, err := c.AssistantSetStatus(ctx, &slack.AssistantSetStatusRequest{
+			_, err := h.client.AssistantSetStatus(ctx, &slack.AssistantSetStatusRequest{
 				Channel:         channelID,
 				Status:          "Preparing magic...",
-				ThreadTimestamp: ec.Event.OfAssistantThreadStarted.AssistantThread.ThreadTimestamp,
+				ThreadTimestamp: threadTS,
 			})
 			return err
 		})
@@ -63,11 +133,30 @@ func (h *Handler) HandleEventsAPI(ctx context.Context, payload *event.EventsAPIP
 
 		time.Sleep(3 * time.Second)
 
+		// Block Kit을 사용한 인사 메시지
 		err = retry.Do(ctx, func(ctx context.Context) error {
-			_, err := c.PostMessage(ctx, &slack.PostMessageRequest{
+			_, err := h.client.PostMessage(ctx, &slack.PostMessageRequest{
 				Channel:         channelID,
-				Text:            "What spell should I cast?",
-				ThreadTimestamp: ec.Event.OfAssistantThreadStarted.AssistantThread.ThreadTimestamp,
+				Text:            "What spell should I cast?", // 폴백 텍스트
+				ThreadTimestamp: threadTS,
+				Blocks: []slack.SlackBlock{
+					{
+						Type: "section",
+						Text: &slack.SlackText{
+							Type: "mrkdwn",
+							Text: "✨ *What spell should I cast?*",
+						},
+					},
+					{
+						Type: "context",
+						Elements: []interface{}{
+							slack.SlackContextElement{
+								Type: "mrkdwn",
+								Text: "_Type your message to get started..._",
+							},
+						},
+					},
+				},
 			})
 			return err
 		})
@@ -98,13 +187,20 @@ func main() {
 		cancel()
 	}()
 
-	c := slack.NewClient(http.DefaultClient, appToken, botToken)
-	resp, err := c.OpenConnection(ctx)
+	client := slack.NewClient(http.DefaultClient, appToken, botToken)
+	resp, err := client.OpenConnection(ctx)
 	if err != nil {
+		slog.Error("failed to open connection", slog.Any("error", err))
 		return
 	}
 
-	b := bot.NewBot(&Handler{appToken: appToken, botToken: botToken})
+	handler := &Handler{
+		appToken: appToken,
+		botToken: botToken,
+		client:   client,
+	}
+
+	b := bot.NewBot(handler)
 	if err := b.Run(ctx, resp.URL); err != nil {
 		slog.Error("failed to run bot", slog.Any("error", err))
 	}
