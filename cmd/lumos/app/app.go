0a3a0476408dade160753a0f812ca986827f31f9
Jonghoon Yoon <rhagodessa@gmail.com>
2025-11-20 16:21:30 +0900

    OpenAI 서비스에 맞게 조정 (#32)

--- cmd/lumos/app/app.go
+++ cmd/lumos/app/app.go
@@ -8,31 +8,65 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
+
 	"github.com/joyfuldevs/project-lumos/pkg/slack/api"
 	"github.com/joyfuldevs/project-lumos/pkg/slack/bot"
 )
 
 func Run() error {
-	appToken, ok := os.LookupEnv("SLACK_APP_TOKEN")
-	if !ok {
-		return errors.New("SLACK_APP_TOKEN is not set")
-	}
-	botToken, ok := os.LookupEnv("SLACK_BOT_TOKEN")
-	if !ok {
-		return errors.New("SLACK_BOT_TOKEN is not set")
-	}
-
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	c := api.NewClient(http.DefaultClient, appToken, botToken)
-	resp, err := c.OpenConnection(ctx)
+	slackClient, err := slackClientFromEnv()
 	if err != nil {
 		return err
 	}
 
-	botHandler := NewBotHandler(c)
+	resp, err := slackClient.OpenConnection(ctx)
+	if err != nil {
+		return err
+	}
+
+	openaiClient, err := openaiClientFromEnv()
+	if err != nil {
+		return err
+	}
+
+	botHandler := NewBotHandler(slackClient, openaiClient)
 	bot := bot.NewBot(botHandler)
 
 	return bot.Run(ctx, resp.URL)
+}
+
+func slackClientFromEnv() (*api.Client, error) {
+	appToken, ok := os.LookupEnv("SLACK_APP_TOKEN")
+	if !ok {
+		return nil, errors.New("SLACK_APP_TOKEN is not set")
+	}
+	botToken, ok := os.LookupEnv("SLACK_BOT_TOKEN")
+	if !ok {
+		return nil, errors.New("SLACK_BOT_TOKEN is not set")
+	}
+
+	client := api.NewClient(http.DefaultClient, appToken, botToken)
+	return client, nil
+}
+
+func openaiClientFromEnv() (*openai.Client, error) {
+	apiURL, ok := os.LookupEnv("OPENAI_API_URL")
+	if !ok {
+		return nil, errors.New("OPENAI_API_URL is not set")
+	}
+	apiKey, ok := os.LookupEnv("OPENAI_API_KEY")
+	if !ok {
+		return nil, errors.New("OPENAI_API_KEY is not set")
+	}
+
+	client := openai.NewClient(
+		option.WithBaseURL(apiURL),
+		option.WithAPIKey(apiKey),
+	)
+	return &client, nil
 }
